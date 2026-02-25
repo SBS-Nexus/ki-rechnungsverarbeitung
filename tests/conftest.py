dce1334b934146c2caf823603fe974690e846ab3
Luis Schenk <luis@schenk.com>
2026-02-25 21:02:56 +0100

    test: Dummy-DB-Engine in CI um begin()-Transaktionen ergänzt

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,20 @@ import pytest
 DUMMY_URL = "postgresql+psycopg://dummy/dummy"
 
 
+class _DummyTransaction:
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc, tb):
+        return False
+
+    def commit(self):
+        pass
+
+    def rollback(self):
+        pass
+
+
 class _DummyConnection:
     def __enter__(self):
         return self
@@ -21,6 +35,9 @@ class _DummyConnection:
 
     def in_transaction(self):
         return False
+
+    def begin(self):
+        return _DummyTransaction()
 
 
 class _DummyEngine:
